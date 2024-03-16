2215e7742230ff70ed4e6306ff88ffe5c1be0040
lnsprnz <linus.prinz@gmx.de>
2024-03-16 18:44:28 +0100

    Create Functions

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+def load_document():
+
+    loader = PyPDFLoader()
+    pages = loader.load_and_split()
+
+    return pages
+
+print(load_document())
+
+def split_document(pages):
+
+    text_chunk = "Example"
+
+    return text_chunk
+def embedding_document (text_chunk):
+
+
+
+
+
+
+
+
+
 
