2011e9a2873df45752324c17220d34da24e3a424
lnsprnz <linus.prinz@gmx.de>
2024-03-31 13:06:55 +0200

    Create Functions

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 from langchain_community.document_loaders import PyPDFLoader
 
+
 def load_document():
+    loader = PyPDFLoader(
+        r"C:\Users\linus\OneDrive\01_Master\TAEM_WING_HSOS\KI im betrieblichen Kontext\Literatur\rili_betriebliche-innovation.pdf")
+    pages = loader.load()
+    for page in pages:
+        print(str(page.metadata["page"]) + ":", page.page_content)
 
-    loader = PyPDFLoader()
-    pages = loader.load_and_split()
-
-    return pages
-
-print(load_document())
 
 def split_document(pages):
-
     text_chunk = "Example"
 
     return text_chunk
-def embedding_document (text_chunk):
 
 
+def embedding_document(text_chunk):
+    return
 
 
-
-
-
-
-
-
+load_document()
